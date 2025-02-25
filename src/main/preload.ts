77d8eca18410f9c032d11f70230d17e83b526423
TNTStudios <tntstudiosn@gmail.com>
2025-02-25 06:44:52 -0600

    update

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -3,5 +3,4 @@ import { contextBridge, ipcRenderer } from "electron";
 contextBridge.exposeInMainWorld("electron", {
     checkFirstRun: () => ipcRenderer.invoke("check-fristrun"),
     setFirstRun: () => ipcRenderer.send("set-fristrun"),
-    setTheme: (theme: string) => ipcRenderer.send("set-theme", theme),
 });
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const logo = document.getElementById("logo");
+    const logo = document.getElementById("logo");
 
-  logo.addEventListener("animationend", async (event) => {
-      if (event.animationName === "logoAnimation") {
-          setTimeout(async () => {
-              const isFirstRun = await window.electron.checkFirstRun();
-              if (isFirstRun) {
-                  window.location.href = "welcome.html";
-              } else {
-                  window.location.href = "main.html";
-              }
-          }, 800); // Espera 800ms antes de redirigir
-      }
-  });
+    logo.addEventListener("animationend", async (event) => {
+        if (event.animationName === "logoAnimation") {
+            setTimeout(async () => {
+                const isFirstRun = await window.electron.checkFirstRun();
+                if (isFirstRun) {
+                    window.location.href = "welcome.html";
+                } else {
+                    window.location.href = "main.html";
+                }
+            }, 800); // Espera 800ms antes de redirigir
+        }
+    });
 });
